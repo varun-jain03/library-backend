261e14ded33d7d56864b634d669b15791d63c5c1
varun jain <varunjain.jain.j@gmail.com>
2025-10-17 21:49:12 +0530

    first commit

---
+++ BackEnd/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+require('dotenv').config();
+const { connectDB } = require('./config/db.js');
+const { authRoutes } = require('./routes/auth.routes.js');
+const { bookRoutes } = require('./routes/book.routes.js');
+const { myBookRoutes } = require('./routes/myBook.routes.js');
+const cors = require('cors');
+
+const PORT = process.env.PORT;
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/auth', authRoutes);
+app.use('/books', bookRoutes);
+app.use('/mybooks', myBookRoutes);
+
+
+
+app.listen(8080, () => {
+    connectDB();
+    console.log('the server is running on http://localhost:8080');
+})
